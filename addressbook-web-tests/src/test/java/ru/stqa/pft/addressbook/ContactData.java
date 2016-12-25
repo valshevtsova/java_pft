e01a24171cf93323717be53afb81a6b329a39ff5
valshevtsova <w4y@tut.by>
2016-12-25 21:17:18 +0300

    Тест создания нового контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String lastname;
+    private final String nickname;
+    private final String homephone;
+    private final String email;
+
+    public ContactData(String firstname, String lastname, String nickname, String homephone, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.homephone = homephone;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getHomephone() {
+        return homephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
