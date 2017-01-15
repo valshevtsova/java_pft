55f1c2a71edbb4370b0e242884d11d90153427b9
valshevtsova <w4y@tut.by>
2017-01-15 20:09:10 +0300

    Циклы и коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,9 +9,11 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
-
+        int before = app.getGroupHelper().getGroupCount();
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(before, after + 1);
 
     }
 
