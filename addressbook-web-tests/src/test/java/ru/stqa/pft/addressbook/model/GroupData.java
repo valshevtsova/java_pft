f5b44afe192512e1e3b5e688a344bfea5cdf1e2f
valshevtsova <w4y@tut.by>
2017-01-20 22:30:45 +0300

    Поиск максимального элемента в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -2,47 +2,28 @@ package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
     private final String name;
-    private final String id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
     private final String header;
     private final String footer;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
 
-        GroupData groupData = (GroupData) o;
-
-        if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
-        return id != null ? id.equals(groupData.id) : groupData.id == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (id != null ? id.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                '}';
-    }
-
-    public String getId() {
+    public int getId() {
         return id;
     }
+
 
     public GroupData(String name, String header, String footer) {
         this.name = name;
         this.header = header;
         this.footer = footer;
-        this.id = null;
+        this.id = 0;
     }
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -59,6 +40,32 @@ public class GroupData {
 
     public String getFooter() {
         return footer;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + id;
+        return result;
     }
 
 }
