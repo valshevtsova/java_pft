cc679a3babf8b460eac011c342bdc621f01b0a32
valshevtsova <w4y@tut.by>
2017-01-28 14:32:48 +0300

    Fluent-интерфейсы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,32 +1,11 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
-    private final String name;
-    private int id;
-    private final String header;
-    private final String footer;
+    private String name;
+    private int id = Integer.MAX_VALUE;;
+    private String header;
+    private String footer;
 
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getId() {
-        return id;
-    }
-
-
-    public GroupData(String name, String header, String footer) {
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
-        this.id = Integer.MAX_VALUE;
-    }
-    public GroupData(int id, String name, String header, String footer) {
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -38,6 +17,30 @@ public class GroupData {
 
     public String getFooter() {
         return footer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public GroupData withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public GroupData withHeader(String header) {
+        this.header = header;
+        return this;
+    }
+
+    public GroupData withFooter(String footer) {
+        this.footer = footer;
+        return this;
+    }
+
+    public GroupData withId(int id) {
+        this.id = id;
+        return this;
     }
 
     @Override
