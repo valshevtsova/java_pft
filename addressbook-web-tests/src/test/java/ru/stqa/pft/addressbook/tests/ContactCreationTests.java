e50d8767ee3027830020a5120e6848a7c0f20141
valshevtsova <w4y@tut.by>
2017-02-02 23:40:47 +0300

    Автоматическое закрытие файлов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -21,16 +21,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
     @DataProvider
     public Iterator<Object[]> validContactsFromJson() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
-        String json = "";
-        String line = reader.readLine();
-        while (line != null){
-            json += line;
-            line = reader.readLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));) {
+            String json = "";
+            String line = reader.readLine();
+            while (line != null){
+                json += line;
+                line = reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<ContactData> groups  = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+            return groups.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
         }
-        Gson gson = new Gson();
-        List<ContactData> groups  = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
-        return groups.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
     }
 
     @Test (dataProvider = "validContactsFromJson")
