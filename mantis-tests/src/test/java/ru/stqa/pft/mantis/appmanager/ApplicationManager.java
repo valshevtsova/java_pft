98cb8c1c2a8354935a45152c57051a9570254072
valshevtsova <w4y@tut.by>
2017-02-12 21:06:30 +0300

    Передача файлов на удалённую машину по протоколу FTP Гиперссылка

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -16,9 +16,11 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
 
     private String browser;
+    private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -28,20 +30,12 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
-        if(Objects.equals(browser, BrowserType.FIREFOX)){
-            wd = new FirefoxDriver();
-        } else if(Objects.equals(browser, BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if(Objects.equals(browser, BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
-        }
-
-        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
     }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
     public HttpSession newSession() {
@@ -50,5 +44,35 @@ public class ApplicationManager {
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if(Objects.equals(browser, BrowserType.FIREFOX)){
+                wd = new FirefoxDriver();
+            } else if(Objects.equals(browser, BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if(Objects.equals(browser, BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
     }
 }
---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,9 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_default_timezone       = 'Europe/Berlin';
+$g_crypto_master_salt     = 'zQE6277zJvyJIBDoQ4ciDxoEmnBUL3+DxJJ5x2IDJYg=';
+$g_signup_use_captcha	  = OFF;
