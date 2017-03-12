7738507d33f9a07f0f9d5c5321b9f7286075143b
valshevtsova <w4y@tut.by>
2017-03-12 22:26:37 +0300

    REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("b39f4881a9d3de6504f9dfefbc58ec9919ae098f");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("valshevtsova", "java_pft")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
